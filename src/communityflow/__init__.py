fab79a78e5c22b31881dc4d59d92d126abb41fab
Michael Uftring <michael.uftring@gmail.com>
2021-03-29 06:30:13 -0400

    refactor: move ColorGrid to its own source file

--- src/communityflow/__init__.py
+++ src/communityflow/__init__.py
@@ -4,3 +4,4 @@ from .graph.graph import create_graphs, create_graph
 from .graph.faster import create_graphs_faster, create_graph_faster
 from .sequence import Sequence
 from .timer import Timer, TimerError
+from .colorgrid import ColorGrid
